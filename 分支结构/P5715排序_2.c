cffb2787367efb24d5cd3ff7ed0b6b7f29fa68e8
Ysola <keivong@outlook.com>
2022-01-29 22:52:24 +0800

    update: add new file
    add P5715 三个数排序

---
+++ 分支结构/P5715排序_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define N 3
+int main(void)
+{
+    int a[N], i, j, t, min;
+    for (i = 0; i < N; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < N; i++)
+    {
+        min = i;
+        for (j = i + 1; j < N; j++)
+        {
+            if (a[min] > a[j])
+            {
+                t = a[min];
+                a[min] = a[j];
+                a[j] = t;
+            }
+        }
+    }
+
+    for (i = 0; i < N; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
