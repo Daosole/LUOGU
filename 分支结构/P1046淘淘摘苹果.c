7dc0f596e43625d9640f614cdf98af77647d7806
Ysola <keivong@outlook.com>
2022-01-30 22:35:20 +0800

    update: add P1046

---
+++ 分支结构/P1046淘淘摘苹果.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+    int a[10], i, h, count = 0;
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    scanf("%d", &h);
+    for (i = 0; i < 10; i++)
+    {
+        if (h + 30 >= a[i])
+            count++;
+    }
+    printf("%d", count);
+    return 0;
+}
