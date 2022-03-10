05da7c86084381da0886ad71c63f8a361ef80742
Daosole <keivong@outlook.com>
2022-03-10 16:32:17 +0800

    update: NOT AC

---
+++ 循环结构/P1009阶乘之和.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(void)
+{
+    long long fac(int x);
+    int n, i;
+    scanf("%d", &n);
+    long long sum = 0;
+    for (i = 1; i <= n; i++)
+    {
+        sum = sum + fac(i);
+    }
+    printf("%lld", sum);
+    return 0;
+}
+long long fac(int x)
+{
+    int i;
+    long long f = 1;
+    if (x == 1)
+        f = 1;
+    else
+    {
+        for (i = 1; i <= x; i++)
+        {
+            f = f * i;
+        }
+    }
+
+    return f; 
+}
