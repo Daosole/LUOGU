a2aff50acf34515cf980a5dba9a37f383b33e4c8
Daosole <keivong@outlook.com>
2022-04-21 22:43:51 +0800

    update: AC
    :smile: :firework:

--- 顺序结构/P2433小学数学N合一.cc
+++ 顺序结构/P2433小学数学N合一.cc
@@ -1,7 +1,7 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <math.h>
-// 填上你觉得需要的其他头文件
+
 using namespace std;
 int main()
 {
@@ -9,12 +9,10 @@ int main()
     cin >> T;
     if (T == 1)
     {
-        // 粘贴问题 1 的主函数代码，除了 return 0
         cout << "I love Luogu!";
     }
     else if (T == 2)
     {
-        // 粘贴问题 2 的主函数代码，除了 return 0
         cout << 2 + 4 << " " << 10 - 2 - 4;
     }
     else if (T == 3)
@@ -26,31 +24,26 @@ int main()
         cout << a << endl;
         cout << b << endl;
         cout << c << endl;
-        // 请自行完成问题 3 的代码
     }
     else if (T == 4)
     {
         double n;
         n = 500 / 3.0;
         cout << setprecision(6) << n;
-        // 请自行完成问题 4 的代码
     }
     else if (T == 5)
     {
         cout << 15;
-        // 请自行完成问题 5 的代码
     }
     else if (T == 6)
     {
-        cout << 10;
-        // 请自行完成问题 6 的代码
+        cout << sqrt(36 + 81);
     }
     else if (T == 7)
     {
         cout << 100 + 10 << endl;
         cout << 100 + 10 - 20 << endl;
         cout << 0 << endl;
-        // 请自行完成问题 7 的代码
     }
     else if (T == 8)
     {
@@ -58,40 +51,34 @@ int main()
         cout << 2 * pi * r << endl;
         cout << pi * r * r << endl;
         cout << (4 * pi * r * r * r) / 3 << endl;
-        // 请自行完成问题 8 的代码
     }
     else if (T == 9)
     {
         cout << 22;
-        // 请自行完成问题 9 的代码
     }
     else if (T == 10)
     {
         cout << 9;
-        // 请自行完成问题 10 的代码
     }
     else if (T == 11)
     {
         cout << 1.0 * 100 / 3;
-        // 请自行完成问题 11 的代码
     }
     else if (T == 12)
     {
         cout << 13 << endl;
-        cout << 'S' << endl;
-        // 请自行完成问题 12 的代码
+        cout << 'R' << endl;
     }
     else if (T == 13)
     {
-        double pi = 3.141593, V;
-        V = 4 * pi * (4 * 4 * 4 + 10 * 10 * 10) / 3;
-        cout << (int)pow(V, 1.0 / 3);
-        // 请自行完成问题 13 的代码
+        // double pi = 3.141593, V;
+        // V = 4 * pi * (4 * 4 * 4 + 10 * 10 * 10) / 3;
+        // cout << (int)pow(V, 1.0 / 3);
+        cout << 16;
     }
     else if (T == 14)
     {
         cout << 50;
-        // 请自行完成问题 14 的代码
     }
     return 0;
 }
