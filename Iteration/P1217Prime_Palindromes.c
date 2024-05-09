e194a569bd5f42551d3e27b423ba2d9f53274a86
wieaqua <keivong@outlook.com>
2024-05-09 09:14:11 +0800

    fix: 通过 :tada:
    通过 AI 和题解帮助，AC nice。之前使用数组判断回文 导致 TLE

--- Iteration/P1217Prime_Palindromes.c
+++ Iteration/P1217Prime_Palindromes.c
@@ -1,61 +1,41 @@
 // 回文质数
+
+/*
+使用暴力方法破解 达不到时间要求 查看题解 
+*/
 #include <stdio.h>
 
 int main(void)
 {
-    unsigned int a, b;
-    scanf("%d %d", &a, &b);
-    int judgePalin(int);
-    int prime(int);
-    for (int i = a; i <= b; i++) {
+    long a, b;
+    scanf("%ld %ld", &a, &b);
+    int is_palindrome(long); // 判断回文
+    int is_prime(long); // 判断素数
+    for (long i = a; i <= b; i++) {
         if (i % 2 == 0)
             continue;
-        if (judgePalin(i) == 1 && prime(i) == 1) {
-            printf("%d\n", i);
+        if (is_palindrome(i) == 1 && is_prime(i) == 1) {
+            printf("%ld\n", i);
         }
     }
 
     return 0;
 }
 
-int judgePalin(int num)
+int is_palindrome(long num)
 {
-    int judgeDigit(int);
-    int n;
-    n = judgeDigit(num);
-    int a[n], b[n];
-    int ta = num;
-    for (int i = 0; i < n; i++) {
-        a[i] = ta % 10;
-        ta /= 10;
-    }
-    for (int i = n - 1; i >= 0; i--) {
-        b[i] = num % 10;
+    long original = num;
+    long sum = 0;
+    while (num > 0) {
+        int digit = num % 10;
+        sum = sum * 10 + digit;
         num /= 10;
     }
-    int j = 0, flag = 1;
-    while (j < n) {
-        if (a[j] == b[j])
-            j++;
-        else {
-            flag = 0;
-            break;
-        }
-    }
 
-    return flag;
+    return original == sum;
 }
 
-int judgeDigit(int jd)
-{
-    int digit = 1;
-    while (jd /= 10)
-        digit++;
-
-    return digit; // 返回位数
-}
-
-int prime(int p)
+int is_prime(long p)
 {
     int flag = 1;
     if (p == 0 || p == 1)
