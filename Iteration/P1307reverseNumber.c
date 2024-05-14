3a783e07f5ff1e0084acc86c9f8e730b9d6bffbb
wieaqua <keivong@outlook.com>
2024-05-14 15:09:01 +0800

    style: add comment

--- Iteration/P1307reverseNumber.c
+++ Iteration/P1307reverseNumber.c
@@ -7,14 +7,14 @@ int main(void)
     int n, i = 0;
     scanf("%d", &n);
     if (n < 0) {
-        printf("-");
-        n = -n;
+        printf("-"); // 如果是负数 先打印负号
+        n = -n;  // 将所有情况按正数处理
     }
     int sum = 0;
     if (n % 10 == 0) {
-        n /= 10;
+        n /= 10; // 处理末尾为 0 的情况
     }
-    while (n != 0) {
+    while (n != 0) {  // 反转
         sum = sum * 10 + n % 10;
         n /= 10;
     }
