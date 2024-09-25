704c9855b9fb3e8acfe9602a39fb33bde9c0ecc6
wieaqua <keivong@outlook.com>
2024-09-25 16:06:34 +0800

    fix: 借助题解 AC :tada:

--- Function_Struct/P5740.cc
+++ Function_Struct/P5740.cc
@@ -1,9 +1,9 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 struct Student {
-    char name[8];
+    string name;
     int yuwen;
     int math;
     int eng;
@@ -19,19 +19,20 @@ int main()
         cin >> stu[i].name >> stu[i].yuwen >> stu[i].math >> stu[i].eng;
         stu[i].sum = stu[i].yuwen + stu[i].math + stu[i].eng;
     }
-    int max_index = 0;
+
     int max = 0;
     for (int i = 0; i < n; i++) {
-        if (max < stu[i].sum) {  // 使用 <= 而不是 < 来处理相同总分的情况
+        if (max < stu[i].sum) { // 使用 <= 而不是 < 来处理相同总分的情况
             max = stu[i].sum;
-            max_index = i;
         }
     }
 
-    cout << stu[max_index].name << " "
-         << stu[max_index].yuwen << " "
-         << stu[max_index].math << " "
-         << stu[max_index].eng;
+    for (int i = 0; i < n; i++) {
+        if (stu[i].sum == max) {
+            cout << stu[i].name << " " << stu[i].yuwen << " "
+                 << stu[i].math << " " << stu[i].eng;
+            return 0;
+        }
+    }
 
-    return 0;
 }
